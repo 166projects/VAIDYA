8d0fdc109fe88bfe91794aabab472015ff75dd26
dudekula.pasha <dudekula.pasha@WKSBAN24TR17009.corp.mphasis.com>
2019-08-07 19:41:02 +0530

    commit

--- OnlineClinic/src/main/java/com/mphasis/training/dao/LeaveDaoImpl.java
+++ OnlineClinic/src/main/java/com/mphasis/training/dao/LeaveDaoImpl.java
@@ -1,12 +1,42 @@
 package com.mphasis.training.dao;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.mphasis.training.entities.Clinic;
 import com.mphasis.training.entities.Leave;
 
 public class LeaveDaoImpl implements LeaveDao {
-
+	@Autowired
+	SessionFactory sessionFactory;
 	public void insertLeave(Leave leave) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	public void approveLeave(Leave leave) {
+		// TODO Auto-generated method stub
+		Session session = sessionFactory.openSession();
+		Transaction tr = session.beginTransaction();
+		Leave leave1= (Leave) session.get(Leave.class, leave.getlId());
+//		leave1.setReason(leave.getReason());
+		leave1.setLeave_status(1);
+		session.update(leave);
+		tr.commit();
+
+	}
+
+	public void denyLeave(Leave leave) {
+		// TODO Auto-generated method stub
+		Session session = sessionFactory.openSession();
+		Transaction tr = session.beginTransaction();
+		Leave leave1= (Leave) session.get(Leave.class, leave.getlId());
+//		leave1.setReason(leave.getReason());
+		leave1.setLeave_status(0);
+		session.update(leave);
+		tr.commit();
 	}
 
 }
--- OnlineClinic/src/main/java/com/mphasis/training/dao/PatientDao.java
+++ OnlineClinic/src/main/java/com/mphasis/training/dao/PatientDao.java
@@ -4,4 +4,5 @@ import com.mphasis.training.entities.Patient;
 
 public interface PatientDao {
 	public void insertPatient(Patient patient);
+	public void updatePatient(int pid);
 }
