53aa95ca9696850e05bda31c2cc29f41a7d0d3ef
Vishnupriyasomisetty <51980723+Vishnupriyasomisetty@users.noreply.github.com>
2019-08-08 14:14:01 +0530

    patient controller added

--- OnlineClinic/src/main/java/com/mphasis/training/controller/PatientController.java
+++ OnlineClinic/src/main/java/com/mphasis/training/controller/PatientController.java
@@ -1,7 +1,73 @@
 
+
 package com.mphasis.training.controller;
 
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mphasis.training.bo.PatientBo;
+
+
+@RestController
+@RequestMapping("/patient")
 public class PatientController {
-	public void update() {
+	@Autowired
+	PatientBo patientBo;
+	@RequestMapping(value="/patients",method=RequestMethod.GET,produces=MediaType.APPLICATION_JSON_VALUE)
+	public List<Patient> getAll()
+	{
+	return patientBo.getAssignments();
+	}
+
+
+@RequestMapping(value="/add",method=RequestMethod.POST,produces=MediaType.APPLICATION_JSON_VALUE)
+public void addPatient(@RequestBody Patient patient)
+{
+ patientBo.insertPatient(patient);
 }
+
+
+@RequestMapping(value="/add",method=RequestMethod.PUT,produces=MediaType.APPLICATION_JSON_VALUE)
+public void updatePatient(@RequestBody Patient patient)
+{
+  Patient patient=patientBo.getPatientsById(patient.getpId());
+	if(patient!=null)
+ patientBo.updatePatient(patient);
+	else
+	throw new BusinessException("no patient found");
+
+}
+
+@RequestMapping(value="/add/{pId}",method=RequestMethod.DELETE,produces=MediaType.APPLICATION_JSON_VALUE)
+public void deletePatient(@PathVariable("pId") int pId)
+{
+
+	patientBo.deletePatient(pId);
+
+}
+
+
+@RequestMapping(value="/patient/{pId}",method=RequestMethod.GET,produces=MediaType.APPLICATION_JSON_VALUE)
+public Patient getpatientsById(@PathVariable("pId") int  pId)
+{
+	
+		 return patientBo.getPatientsById(pId);
+
+}
+
+@RequestMapping(value="/patient/{firstname}",method=RequestMethod.GET,produces=MediaType.APPLICATION_JSON_VALUE)
+public Patient getPatientByName(@PathVariable("firstname") String  firstname)
+{
+	
+		 return patientBo.getPatientByName(firstname);
+
+}
+
+
 }
